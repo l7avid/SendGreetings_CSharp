b3cdd6ea606153c95102a25a3d06c2737c715f0e
David Reina <davidereina@hotmail.com>
2023-10-05 15:27:10 -0500

    Simple program to send greetings to three people

---
+++ ChallengeMethods/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace ChallengeMethods
+{
+    internal class Program
+    {
+        static void SendGreetings(string name)
+        {
+            Console.WriteLine($"Hello {name}, I love you!");
+        }
+        static void Main(string[] args)
+        {
+            string daugther = "Majo";
+            string wife = "Lina";
+            string husband = "David";
+            SendGreetings(daugther);
+            SendGreetings(wife);
+            SendGreetings(husband);
+        }
+    }
+}
